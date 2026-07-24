c389c9495ca0cf8cc65c7cf2a8d11a66b1cd53d0
Apratim Mitra <apratim.mitra@nih.gov>
2026-07-24 12:04:51 -0400

    switch frm name to text fr hover tooltip

--- R/functions.R
+++ R/functions.R
@@ -614,7 +614,7 @@ getcountplot <- function(df, intgroup='group', factor.levels, title=NULL,
   ymin <- ifelse(is.null(ymin), min(df$count), ymin)
   ymax <- ifelse(is.null(ymax), max(df$count), ymax)
 
-  p <- ggplot(df, aes(y=.data$count, x=.data[[ intgroup ]], color=.data[[ color ]], name=.data$sample)) +
+  p <- ggplot(df, aes(y=.data$count, x=.data[[ intgroup ]], color=.data[[ color ]], text=paste('sample:', .data$sample))) +
     geom_point(position=position_jitterdodge(dodge.width=0.2),
                  size=2, alpha=0.5)
 
