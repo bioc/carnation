2975d9ff8ede3f3a7834860311138360bc23dd3a
Apratim Mitra <apratim.mitra@nih.gov>
2026-07-30 16:52:14 -0400

    add sample selector to gene plt

--- R/gene-plot.R
+++ R/gene-plot.R
@@ -242,6 +242,31 @@ genePlotUI <- function(id, panel){
 
           ), # bsCollapsePanel
 
+          bsCollapsePanel('sample settings',
+
+            fluidRow(
+              column(4, 'samples'),
+              column(8,
+                selectizeInput(ns('sample_levels'),
+                               label=NULL,
+                               choices=NULL,
+                               selected=NULL,
+                               multiple=TRUE
+                ) # selectizeInput
+              ) # column
+            ), # fluidRow
+            fluidRow(
+              column(6, align='center',
+                     style='margin-bottom: 10px;',
+                actionButton(ns('sample_all'), 'Select all')
+              ), # column
+              column(6, align='center',
+                     style='margin-bottom: 10px;',
+                actionButton(ns('sample_none'), 'Select none')
+              ) # column
+            ) # fluidRow
+          ), # bsCollapsePanel
+
           bsCollapsePanel('More options',
 
             fluidRow(
@@ -430,7 +455,21 @@ genePlotServer <- function(id, obj,
             else if(input$norm_method == 'libsize') rld.i <- app_object()$all_dds
         }
 
+        updateSelectizeInput(session, 'sample_levels',
+                             choices=colnames(rld.i),
+                             selected=colnames(rld.i))
+
         gene_plot_data$all <- rld.i
+      })
+
+      observeEvent(input$sample_all, {
+        updateSelectizeInput(session, 'sample_levels',
+                             selected=colnames(gene_plot_data$all))
+      })
+
+      observeEvent(input$sample_none, {
+        updateSelectizeInput(session, 'sample_levels',
+                             selected='')
       })
 
       # update gene plot controls when col.data changes
@@ -833,6 +872,10 @@ genePlotServer <- function(id, obj,
         # get counts for the genes
         df <- get_gene_counts(rld.i, g, input$xvar,
                               norm_method=input$norm_method)
+
+        if(!is.null(input$sample_levels) & all(input$sample_levels %in% df[['sample']])){
+          df <- df[df$sample %in% input$sample_levels, ]
+        }
 
         # add pseudocount
         pseudocount <- config()$server$de_analysis$gene_plot$pseudocount
